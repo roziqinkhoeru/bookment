ecb3450564034a33168404c4bac9af980b92fbec
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-06-17 20:23:18 +0700

    update controllers pages

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -8,4 +8,19 @@ class Pages extends BaseController
     {
         return view('user/index');
     }
+
+    public function book()
+    {
+        return view('user/book');
+    }
+
+    public function customer()
+    {
+        return view('user/customer');
+    }
+
+    public function cart()
+    {
+        return view('user/chart');
+    }
 }
