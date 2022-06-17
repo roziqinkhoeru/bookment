edd2319e9fee7578f4a55a359f282cc7fbdd5500
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-06-17 23:56:22 +0700

    update book page

--- app/Views/user/book.php
+++ app/Views/user/book.php
@@ -5,30 +5,33 @@
 <section class="container">
     <div class="card m-5">
         <div class="card-header">
-            <div class="row">
-                <div class="col">Books Data</div>
-                <div class="col text-end">
-                    <a href="#" class="btn btn-success btn-sm">Add</a>
+            <div class="w-100 d-flex justify-content-between">
+                <div>Books Data</div>
+                <div>
+                    <a href="#" class="btn btn-success btn-sm d-flex align-items-center"><i class="bi bi-plus"
+                            style="font-size: 1.25rem; margin-right: 0.25rem;"></i><span>Add Buku</span></a>
                 </div>
             </div>
         </div>
         <div class="card-body">
-            <table class="table table-striped">
+            <table class="table table-striped table-bordered">
                 <tr>
-                    <th>ISBN</th>
-                    <th>Author</th>
-                    <th>Title</th>
+                    <th class="text-center">ISBN</th>
+                    <th class="text-center">Author</th>
+                    <th class="text-center">Title</th>
                     <th>Price</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
+                    <th class="text-center">Aksi</th>
                 </tr>
                 <tr>
-                    <td>1</td>
-                    <td>Ismail</td>
-                    <td>Buku ismail</td>
+                    <td class="text-center">1</td>
+                    <td class="text-center">Ismail</td>
+                    <td class="text-center">Buku ismail</td>
                     <td>100000</td>
-                    <td><a href="#" class="btn btn-sm btn-warning">Edit</a></td>
-                    <td><button type="button" onclick="" class="btn btn-danger btn-sm">Delete</button></td>
+                    <td class="text-center"><a href="#" class="btn btn-sm btn-warning me-2"><i
+                                class="bi bi-pencil-square"></i></a>
+                        <button type="button" onclick="" class="btn btn-danger btn-sm"><i
+                                class="bi bi-trash-fill"></i></button>
+                    </td>
                 </tr>
             </table>
         </div>
